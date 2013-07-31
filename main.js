e035f5ddd39ec939610ee846bfe5eb1d1d778d55
Claire Rebello <cr@belugalearning.com>
2013-07-31 12:28:07 +0100

    use hardcoded question settings to test setQuestion (initial bars/dropzones/bars in dropzones)

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ define(['exports', 'cocos2d', 'qlayer', 'bldrawnode', 'polygonclip', 'toollayer'
     var DROPZONE_Z = 1;
     var DRAGGABLE_Z = 1;
 
+    var barheight = 50;
+
     var unitlength = 50;
     var homescale = 0.5;
 
@@ -33,7 +35,114 @@ define(['exports', 'cocos2d', 'qlayer', 'bldrawnode', 'polygonclip', 'toollayer'
 
             this.setBackground(bl.getResource('deep_water_background'))
 
-            this.setQuestion()
+            var question = {
+                bars:[
+                    {
+                        unit: 1,
+                        quantity: 10,
+                        colour: { r: 231, g: 0,     b: 0,   a: 255 }
+                    },
+                    {
+                        unit: 2,
+                        quantity: 5,
+                        colour: { r: 245, g: 94,    b: 0,   a: 255 }
+                    },
+                    {
+                        unit: 3,
+                        quantity: 3,
+                        colour: { r: 247, g: 204,   b: 0,   a: 255 }
+                    },
+                    {
+                        unit: 4,
+                        quantity: 2,
+                        colour: { r: 0,   g: 183,   b: 0,   a: 255 }
+                    },
+                    {
+                        unit: 5,
+                        quantity: 2,
+                        colour: { r: 0,   g: 170,   b: 234, a: 255 }
+                    },
+                    {
+                        unit: 6,
+                        quantity: 1,
+                        colour: { r: 98,  g: 0,     b: 245, a: 255 }
+                    },
+                    {
+                        unit: 7,
+                        quantity: 1,
+                        colour: { r: 225, g: 116,   b: 172, a: 255 }
+                    },
+                    {
+                        unit: 8,
+                        quantity: 1,
+                        colour: { r: 0, g: 0,   b: 0, a: 255 }
+                    },
+                    {
+                        unit: 9,
+                        quantity: 1,
+                        colour: { r: 255, g: 255,   b: 255, a: 255 }
+                    },
+                    {
+                        unit: 10,
+                        quantity: 1,
+                        colour: { r: 150, g: 150,   b: 150, a: 255 }
+                    }
+
+                ],
+
+                containers:[
+                    {
+                        unit: 10,
+                    },
+                    {
+                        unit: 12
+                    },
+                    {
+                        unit: 7
+                    },
+                    {
+                        unit: 10
+                    },
+                    {
+                        unit: 10
+                    },
+                    {
+                        unit: 10
+                    },
+                    {
+                        unit: 10
+                    },
+                    {
+                        unit: 10
+                    },
+                    {
+                        unit: 10
+                    }                     
+                ],
+
+                barsInDropZone:[
+                    {
+                        unit: 1,
+                        dropzone: 1,
+                        colour: { r: 231, g: 0,     b: 0,   a: 255 }
+                    },
+                    {
+                        unit: 2,
+                        dropzone: 1,
+                        colour: { r: 245, g: 94,    b: 0,   a: 255 }
+                    },
+                    {
+                        unit: 1,
+                        dropzone: 2,
+                        colour: { r: 231, g: 0,     b: 0,   a: 255 }
+                    }
+
+                ]
+
+            }
+
+
+            this.setQuestion(question)
             return this;
         },
 
@@ -83,19 +192,13 @@ define(['exports', 'cocos2d', 'qlayer', 'bldrawnode', 'polygonclip', 'toollayer'
                     self._draggableCounter++;
                 }
                 self._prevDraggable = draggable.tag;
-
-                //if in a dropzone
-                //remove it from filled array & filled length
-                //shift everything else right
-
-
             });
 
             dg.onMoveEnded(function (position, draggable) {
  				var dropZones = self.getControls(DROPZONE_PREFIX);
                 var oldDropZone,
                     newDropZone;
-                
+
                 for (var i = 0; i < dropZones.length; i++) {
                     var dropZone = dropZones[i];
                     if (!oldDropZone) {
@@ -131,29 +234,32 @@ define(['exports', 'cocos2d', 'qlayer', 'bldrawnode', 'polygonclip', 'toollayer'
 
                 if (moveToNewDropZone) {
                     var dropZonePos = newDropZone.getPosition();
+                    
                     draggable.setPosition(cc.p(
                         dropZonePos.x + newDropZone._filled * unitlength,
                         dropZonePos.y), true);
+
                     newDropZone._filledArray.push(draggable);
                     newDropZone._filled += draggable._length;
                     draggable.setScale(1);
+
                 } else if (oldDropZone && newDropZone) {
+                    //put it back in its old drop zone
                     var oldDropZonePos = oldDropZone.getPosition();
+                    
                     draggable.setPosition(cc.p(
                         oldDropZonePos.x + oldDropZone._filled * unitlength,
                         oldDropZonePos.y), true);
+                    
                     oldDropZone._filledArray.push(draggable);
                     oldDropZone._filled += draggable._length;
                     draggable.setScale(1);
-                            // draggable.setPosition(draggable._lastPosition);
-                            // draggable.setScale(1);
+                    
                 } else {
-                    // send to prev pos
+                    // send to home
                     draggable.setPosition(draggable._homePosition);
                     draggable.setScale(homescale);
                 }
-                
-                   
             });
 
             this._draggableLayer.addChild(dg);
@@ -165,17 +271,27 @@ define(['exports', 'cocos2d', 'qlayer', 'bldrawnode', 'polygonclip', 'toollayer'
         initCagedBars: function (){
             //read in matrix with length + quantity columns
 
-            //for each row:
-            //make background
-            //call addNumberBondsBar 
 
+            for (i = 0; i<quantity; i++){
+                //for each row - 
+                //make background resource
+                var l = new cc.LayerColor();
+                l.init(colour[length], unitlength * (length), barheight);
+                
+                //call addNumberBondsBar 
+                var dg = self.addNumberBondsBar((i + 1), cc.p(10, (barheight + 5) * (i + 1)), l);
+
+            }      
         },
 
         initDropZoneBars: function (){
             //read in matrix with lengths + dropzones
 
+            //work out position
+            //make background resource
             //call addNumberBondsBar with different position
-            //overwrite homePosition
+            //overwrite homePosition? (have a feeling this is necessary? need to know which cage it belongs to?)
+            //edit filledArray for dropzone
 
         },
         
@@ -208,7 +324,9 @@ define(['exports', 'cocos2d', 'qlayer', 'bldrawnode', 'polygonclip', 'toollayer'
 
         initDropZones: function (){
             //read in array with lengths
-
+            //make array of dropzones
+            //calculate position
+            //calculate vertices of shape
             //call addDropZone
 
         },
@@ -218,51 +336,101 @@ define(['exports', 'cocos2d', 'qlayer', 'bldrawnode', 'polygonclip', 'toollayer'
             throw {name : "NotImplementedError", message : "This needs implementing"};
         },
         
-        setQuestion: function () {
+        setQuestion: function (question) {
             var self = this;
 
-            var colours = [
-                { r: 231, g: 0,   b: 0,   a: 255 },
-                { r: 245, g: 94, b: 0,   a: 255 },
-                { r: 247, g: 204, b: 0,   a: 255 },
-                { r: 0,   g: 183, b: 0,   a: 255 },
-                { r: 0,   g: 170, b: 234, a: 255 },
-                { r: 98,   g: 0, b: 245, a: 255 },
-                { r: 225, g: 116, b: 172, a: 255 }];
+            //add dropzone
+            var margin = (600 - barheight * question.containers.length)/(question.containers.length + 1);
+            console.log(question.containers.length, margin);
 
-            // add bars 
-            this._draggableLayer = DraggableLayer.create();
-            self.addChild(self._draggableLayer);
+            _.each(question.containers, function (container, i){
 
-            _.each(colours, function (colour, i) {
-                for(var j = 1; j < 3; j++){
-                    var l = new cc.LayerColor();
-                    l.init(colour, unitlength * (i + 1), 40);
-                    
-                    var dg = self.addNumberBondsBar((i + 1), cc.p(10, (unitlength + 5) * (i + 1)), l);
-                }
-            });
-            
-         //add dropzone                       
-            var dz = this.addDropZone({
-                        x:400, y:100},
-                        [{x:0, y:0}, {x:0, y:100}, {x:500, y:100}, {x:500, y:0}],
+                var dz = self.addDropZone({
+                            x:400, y:margin + i * (barheight + margin)},
+                            [
+                                {x:0, y:0},
+                                {x:0, y:barheight},
+                                {x:unitlength * container.unit, y:barheight},
+                                {x:unitlength * container.unit, y:0}
+                            ],
                         '');
                     dz._label.setPosition(cc.p(0,0));
                     dz._label.setFontSize(50);
                     dz._filled = 0;
                     dz._filledArray = new Array(),
-                    dz._length = 10;
+                    dz._length = container.unit;
+            });
+       
+            // add bars 
+            this._draggableLayer = DraggableLayer.create();
+            self.addChild(self._draggableLayer);
 
-            var dz1 = this.addDropZone({
-                    x:400, y:300},
-                    [{x:0, y:0}, {x:0, y:100}, {x:500, y:100}, {x:500, y:0}],
-                    '');
-                dz1._label.setPosition(cc.p(0,0));
-                dz1._label.setFontSize(50);
-                dz1._filled = 0;
-                dz1._filledArray = new Array(),
-                dz1._length = 10;
+            _.each(question.bars, function (bar, i) {
+                console.log(bar, i);
+                for(var j = 0; j < bar.quantity; j++){
+                    var l = new cc.LayerColor();
+                    l.init(bar.colour, unitlength * bar.unit, barheight);
+                    
+                    var dg = self.addNumberBondsBar(bar.unit, cc.p(10, (barheight + 5) * bar.unit), l);
+                }
+            
+            });
+            
+            
+
+            //add bars in dropzone
+
+            _.each(question.barsInDropZone, function (bar, i){
+                //find dropzone to put it in
+                var dropZones = self.getControls(DROPZONE_PREFIX);
+                var dropZone = dropZones[bar.dropzone - 1];
+                var dropZonePos = dropZone.getPosition();
+
+                //make bar
+                console.log(bar, i, "making dropzone bar");
+                var l = new cc.LayerColor();
+                l.init(bar.colour, unitlength * bar.unit, barheight);
+
+                
+                var dg = self.addNumberBondsBar(bar.unit, cc.p(dropZonePos.x + (dropZone._filled +bar.unit/2)* unitlength, dropZonePos.y + barheight/2), l);
+                console.log(bar, i, "added bar to dropzone");
+
+                //add to filledArray etc
+                dropZone._filledArray.push(dg);
+                dropZone._filled += dg._length;
+                dg.setScale(1);               
+            });
+
+
+            // var dz = this.addDropZone({
+            //             x:400, y:100},
+            //                 [
+            //                     {x:0, y:0},
+            //                     {x:0, y:barheight + cagepadding},
+            //                     {x:unitlength * cagelength + cagepadding, y:barheight + cagepadding},
+            //                     {x:unitlength * cagelength + cagepadding, y:0}
+            //                 ],
+            //             '');
+            //         dz._label.setPosition(cc.p(0,0));
+            //         dz._label.setFontSize(50);
+            //         dz._filled = 0;
+            //         dz._filledArray = new Array(),
+            //         dz._length = 10;
+
+            // var dz1 = this.addDropZone({
+            //         x:400, y:300},
+            //             [
+            //                 {x:0, y:0}, 
+            //                 {x:0, y:barheight + cagepadding},
+            //                 {x:unitlength * cagelength + cagepadding, y:barheight + cagepadding},
+            //                 {x:unitlength * cagelength + cagepadding, y:0}
+            //             ],
+            //         '');
+            //     dz1._label.setPosition(cc.p(0,0));
+            //     dz1._label.setFontSize(50);
+            //     dz1._filled = 0;
+            //     dz1._filledArray = new Array(),
+            //     dz1._length = 10;
             }
         });
 
@@ -279,7 +447,7 @@ define(['exports', 'cocos2d', 'qlayer', 'bldrawnode', 'polygonclip', 'toollayer'
         var layer = ToolLayer.create();
         scene.addChild(layer);
 
-        scene.layer=layer;
+        scene.layer = layer;
 
         scene.ql = new QLayer();
         scene.ql.init();
